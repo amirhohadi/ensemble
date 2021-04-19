52c6f50184517d3f3f1c24b766564f21a1c994aa
Amir Hossein <amir.h.ohadi@gmail.com>
2021-04-19 20:36:41 +0430

    Complete Implementation of Bagging Model

--- Bagging.py
+++ Bagging.py
@@ -2,6 +2,7 @@ from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 from EnsembleBase import Ensemble
 
+#Bootstrap Aggregation Model
 class BaggingModel(Ensemble):
 
     def __init__(self,classifier_counts=10):
@@ -18,7 +19,12 @@ class BaggingModel(Ensemble):
             self.hypothesises.append(hypothesis)
 
     def classification(self,x):
-        pass
+        outputs = []
+        for i in range(0,self.classifier_counts):
+            out = self.hypothesises[i].predict(x.reshape((1,-1)))
+            outputs.append(out[0])
+        #print(outputs)
+        return max(set(outputs),key=outputs.count)
 
     def bootstrap(self,data):
         bootstraped_data = np.zeros(data.shape)
@@ -28,4 +34,27 @@ class BaggingModel(Ensemble):
             bootstraped_data[i,:] = data[rand_index,:]
 
         return bootstraped_data
+
+    def accuracy(self,testdata):
+        correct_predict = 0
+        wrong_predict = 0
+        for i in range(0,len(testdata)):
+            yhat = self.classification(testdata[i,:-1])
+            y = testdata[i,-1]
+            if yhat == y :
+                correct_predict += 1
+            else:
+                wrong_predict += 1
+        print(f'Correct Predict {correct_predict}, Wrong Predict {wrong_predict}')
+        calculated_accuray = (correct_predict / len(testdata)) * 100
+        print(f'Accuracy is {calculated_accuray}')
+
+
+if __name__ == '__main__':
+    print('Testing Bagging Model With Glass Dataset')
+    dataset = np.genfromtxt('Datasets/Wine.txt',delimiter=',')
+    model = BaggingModel()
+    model.train(dataset)
+    testdataset = dataset[:,:-1]
+    model.accuracy(dataset)
 
